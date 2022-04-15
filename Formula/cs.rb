49b429339c4cbc2abb4b8b3ed2d3cfeee7eedaab
yi.liu <yi.liu@huolala.cn>
2022-04-15 17:07:37 +0800

    cs 0.0.1

---
+++ Formula/cs.rb
@@ -0,0 +1,10 @@
+class CS < Formula
+    desc "config file to golang struct ,support json,yaml,yml,toml"
+    homepage "https://github.com/jiy1012/cs"
+    url "https://github.com/jiy1012/cs/releases/download/v0.0.1/cs-mac.tar.gz"
+    sha256 "108e64989c5397cd35e0338593be707bd5dfd7c887b27eb5531ddf019dfbea69"
+    version "0.0.1"
+    def install
+      bin.install "cs
+    end
+  end
