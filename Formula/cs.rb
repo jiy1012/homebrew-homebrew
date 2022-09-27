5093f70b8371fa16f2b84c5070e2a0196d8ecb80
yi.liu <yi.liu@huolala.cn>
2022-09-27 17:15:17 +0800

    update v0.0.2

--- Formula/cs.rb
+++ Formula/cs.rb
@@ -3,7 +3,7 @@ class Cs < Formula
     homepage "https://github.com/jiy1012/cs"
     url "https://github.com/jiy1012/cs/releases/download/v0.0.2/cs-mac.tar.gz"
     sha256 "82bd3350294e28123f45bd29c1bdf0e037bec76c29efb8b6498626d42d25e137"
-    version "0.0.1"
+    version "0.0.2"
     def install
       bin.install "cs"
     end
