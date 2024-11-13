b11e27872e36645bf2d3f276dd35292741489b7c
刘神马 <jiy1012@163.com>
2024-11-13 16:19:54 +0800

    Update cs.rb to v0.0.4

--- Formula/cs.rb
+++ Formula/cs.rb
@@ -1,9 +1,9 @@
 class Cs < Formula
     desc "config file to golang struct ,support json,yaml,yml,toml"
     homepage "https://github.com/jiy1012/cs"
-    url "https://github.com/jiy1012/cs/releases/download/v0.0.3/cs-mac.tar.gz"
-    sha256 "a5d377bcebef18e69ec24a130fb9bad3cc9434a37698b5ab3ed38c5cb7377242"
-    version "0.0.3"
+    url "https://github.com/jiy1012/cs/releases/download/v0.0.4/cs-mac.tar.gz"
+    sha256 "ef118446381ef666f4f5d1b0841dbd346868607e8fd168fc40c79ff1a5e9621f"
+    version "0.0.4"
     def install
       bin.install "cs"
     end
